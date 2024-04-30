8bd7e0dff3460f8bfd896462e60345540a79e4f3
Pranith Gunda <pranith0527@yahoo.com>
2024-04-30 01:15:58 -0400

    Defined functions for handleAddTask and createTaskCard. Functions are working as expected, will further optimize

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -2,6 +2,7 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 const submitTaskButtonEl = $('#add-tasks');
+const toDoCardsEl = $('#todo-cards'); 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
 
@@ -9,6 +10,52 @@ function generateTaskId() {
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+    // Create div for Card
+    const cardEl = $('<div>');
+    cardEl.addClass('card text-white text-center bg-danger mb-3');
+    
+    // Create div for Card Header
+    const cardHeaderEl = $('<div>');
+    cardHeaderEl.addClass('card-header');
+    cardHeaderEl.text(task.title);
+
+    // Append Card Header to Card
+    cardEl.append(cardHeaderEl);
+
+    // Create div for Card Body
+    const cardBodyEl = $('<div>');
+    cardBodyEl.addClass('card-body');
+
+    // Create paragraph for Due Date
+    const pElDate = $('<p>');
+    pElDate.addClass('card-text');
+    pElDate.text(task.dueDate);
+
+    // Append Paragraph element to Card Body to display Task Due Date
+    cardBodyEl.append(pElDate);
+
+    // Create paragraph for Task Description
+    const pElDesc = $('<p>');
+    pElDesc.addClass('card-text');
+    pElDesc.text(task.description);
+
+    // Append Paragraph element to Card Body to display Task Description
+    cardBodyEl.append(pElDesc);
+
+    // Create element for Delete Button
+    const deleteButtonEl = $('<button>');
+    deleteButtonEl.addClass('btn btn-danger btn-outline-light');
+    deleteButtonEl.text('Delete');
+
+    // Append Delete Button to Card Body El
+    cardBodyEl.append(deleteButtonEl);
+    
+    // Append Card Body to Card El
+    cardEl.append(cardBodyEl);
+
+    // Append Card to ToDo Swim Lane
+
+    toDoCardsEl.append(cardEl);
 
 }
 
@@ -23,22 +70,19 @@ function handleAddTask(event){
     const taskTitle=$('#taskTitle').val();
     const taskDueDate=$('#taskDueDate').val();
     const taskDescription=$('#taskDescription').val();
-    
-    
-    // Close/hide the modal
-    $('#formModal').modal('hide');
-
-    const taskInfo = {
+     
+     // Close/hide the modal
+     $('#formModal').modal('hide');
+     
+     const taskInfo = {
         title:taskTitle,
         dueDate:taskDueDate,
         description:taskDescription
     };
-
-    // For debug
-
-    console.log(taskInfo);
-
-
+    
+    // Call createTaskCard function
+    
+    createTaskCard(taskInfo);
 }
 
 // Todo: create a function to handle deleting a task
